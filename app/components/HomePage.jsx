38c46c6a6efd0559e3f058fb6237ac830c3eff2b
Abass Olanrewaju <abassolanrewaju111@gmail.com>
2023-08-01 18:49:12 +0100

    test: write test for homepage

--- app/components/HomePage.jsx
+++ app/components/HomePage.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 function HomeComponent() {
     return (
-        <div>
-            This is Home
-        </div>
+        <header>
+            <h2 role="heading">
+                This is Home
+            </h2>
+        </header>
     )
 }
 
--- app/page.js
+++ app/page.js
@@ -2,9 +2,7 @@ import HomeComponent from "./components/HomePage"
 
 function HomePage() {
   return (
-    <div>
-      <HomeComponent />
-    </div>
+    <HomeComponent />
   )
 }
 
