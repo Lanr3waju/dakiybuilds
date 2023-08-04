8ac2fe73ba0b0d76ba4e82c597b2531933630242
Abass Olanrewaju <abassolanrewaju111@gmail.com>
2023-08-04 10:12:26 +0100

    feat: create homepage ui

--- app/components/HomePageLoginForm.jsx
+++ app/components/HomePageLoginForm.jsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 function HomePageLoginForm() {
     return (
         <section className="mt-5 max-w-xl rounded-md bg-base-200 px-2 py-9 leading-loose text-base-content shadow-md shadow-base-300 md:w-3/6">
-            <h2 className="font-Poppins text-lg font-semibold text-base-content/70">Login to access your projects, or <Link className="text-info" href="/#">create new account:</Link></h2>
+            <h2 className="font-Poppins text-lg font-semibold text-base-content/70">Login to access your projects, or <Link className="link link-primary" href="/#">create new account:</Link></h2>
             <form className="flex flex-col">
                 <input type="email" placeholder="Enter your mail" className="input input-bordered input-primary my-3 w-full focus:border-none focus:ring-0" />
                 <input type="password" placeholder="Enter your password" className="input input-bordered input-primary my-1 w-full focus:border-none focus:ring-0" />
