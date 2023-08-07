f99f2a6a2cafed6094a171d40d587ae7191561ce
Abass Abdul Wasi Olanrewaju <abassolanrewaju111@gmail.com>
2023-08-07 13:44:55 +0100

    feat: add-docs-page-ui (#14)

--- app/__tests__/projectDocuments/index.test.jsx
+++ app/__tests__/projectDocuments/index.test.jsx
@@ -5,5 +5,4 @@ test('renders the Home header with correct text', () => {
     render(<ProjectDocuments />)
     const headingElement = screen.getByTestId('project-documents-heading')
     expect(headingElement).toBeInTheDocument()
-    expect(headingElement).toHaveTextContent('This is project docs')
 })
--- app/components/dashboard/greeting/Greeting.jsx
+++ app/components/dashboard/greeting/Greeting.jsx
@@ -2,8 +2,8 @@ import { getGreeting } from "./getTime"
 
 function Greeting() {
     return (
-        <h2 className="mt-5 text-xl font-semibold">
-            Hello Bldr. Abass, {getGreeting()}.
+        <h2 className="mt-4 text-xl font-semibold">
+            Hello Mr. Abass, {getGreeting()}.
         </h2>
     )
 }
