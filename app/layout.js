c8a20d0cace4e1bee6849249217edb89577fbf17
Abass Olanrewaju <abassolanrewaju111@gmail.com>
2023-08-10 20:12:44 +0100

    fix: remove metadat from layout component

--- app/layout.js
+++ app/layout.js
@@ -3,12 +3,6 @@ import { usePathname } from 'next/navigation'
 import Header from './components/layout/Header'
 import './styles/globals.css'
 
-export const metadata = {
-  title: 'dakiybuilds',
-  description: 'A web-based construction management system',
-  keywords: 'web development, web-based, construction management, construction management system, project management, construction industry, supabase, next.js, html, css'
-}
-
 export default function RootLayout({ children }) {
   const pathname = usePathname()
   if (pathname !== "/")
