9ddee0cc2f69fe50d3716ec08d29ce4b1a550cab
Abass Olanrewaju <abassolanrewaju111@gmail.com>
2023-08-17 18:26:49 +0100

    fix: remove redundant test suites

--- app/__tests__/projectSettings/index.test.jsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
-import ProjectSettings from '@/app/project-settings/page'
-test('renders the Home header with correct text', () => {
-    render(<ProjectSettings />)
-    const headingElement = screen.getByTestId('project-settings-heading')
-    expect(headingElement).toBeInTheDocument()
-    expect(headingElement).toHaveTextContent('This is project settings')
-})
