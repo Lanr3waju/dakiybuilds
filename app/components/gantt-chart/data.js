add7dd5883cd34ecb8884612ef34d9fffe720733
Abass Abdul Wasi Olanrewaju <abassolanrewaju111@gmail.com>
2023-12-19 11:03:13 +0100

    feat: made changes to resource data (#56)

--- app/components/gantt-chart/data.js
+++ app/components/gantt-chart/data.js
@@ -7,7 +7,7 @@ export const editingData = [
   {
     TaskID: 1,
     TaskName: 'Dummy Task ',
-    StartDate: new Date('11/23/2023'),
-    EndDate: new Date('12/02/2023'),
+    // StartDate: new Date('11/23/2023'),
+    // EndDate: new Date('12/02/2023'),
   },
 ]
