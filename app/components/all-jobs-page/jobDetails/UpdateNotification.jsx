8b635f73151e4bd227b2995cb23f359600788bbf
Abass Olanrewaju <abassolanrewaju111@gmail.com>
2024-01-03 12:47:25 +0100

    feat: add alert for project updates

--- app/components/all-jobs-page/jobDetails/UpdateNotification.jsx
+++ app/components/all-jobs-page/jobDetails/UpdateNotification.jsx
@@ -1,7 +1,25 @@
+import { useState, useEffect } from 'react';
+
 function UpdateNotification() {
+    // Initialize a state variable to manage the visibility of the animate-bounce class.
+    const [showBounceAnimation, setShowBounceAnimation] = useState(true)
+
+    useEffect(() => {
+        // Set a timeout to remove the animate-bounce class after 10 seconds.
+        const timer = setTimeout(() => {
+            setShowBounceAnimation(false)
+        }, 10000) // 10 seconds in milliseconds
+
+        // Clear the timeout if the component unmounts or the condition changes.
+        return () => clearTimeout(timer)
+    }, []) // Empty dependency array ensures that this effect runs once after the initial render.
+
+    // Conditionally apply the animate-bounce class based on the state variable.
+    const animationClass = showBounceAnimation ? 'animate-bounce' : '';
+
     return (
         <section className="p-4 border-4 border-warning flex md:flex-row flex-col md:justify-evenly md:items-center rounded-lg m-3 cursor-pointer">
-            <div role="alert" className="alert shadow-lg alert-warning text-warning-content animate-bounce">
+            <div role="alert" className={`alert shadow-lg alert-warning text-warning-content ${animationClass}`}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                 <div>
                     <h3 className="font-bold">Progress Updates!</h3>
@@ -9,7 +27,6 @@ function UpdateNotification() {
                 </div>
             </div>
         </section>
-
     )
 }
 
