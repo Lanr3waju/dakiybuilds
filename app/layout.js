07397324bd967a0d7fa066176abe2fcdf67096f6
Abass Olanrewaju <abassolanrewaju111@gmail.com>
2024-01-04 11:51:54 +0100

    style: enable cupcake theme for testing

--- app/layout.js
+++ app/layout.js
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
   }
 
   return (
-    <html lang="en" data-theme="valentine" className={bodyClass}>
+    <html lang="en" data-theme="cupcake" className={bodyClass}>
       <head>
         <title>{title}</title>
       </head>
