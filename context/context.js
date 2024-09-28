02c7e31f60b817fadb96ffed8416530e86f7f5cb
Abass Olanrewaju <abassolanrewaju111@gmail.com>
2024-09-28 22:48:11 +0100

    fix: optimise context

--- context/context.js
+++ context/context.js
@@ -1,192 +1,126 @@
 'use client'
+
 import { createContext, useEffect, useState } from 'react'
 import { fetchCurrentProjectId, getAppTheme, getProjects } from './supabaseTables'
 import { getExpendituresByProjectId } from '@/app/components/project-finance/supabaseTables'
 
+// Create a context for managing and sharing state across components
 export const DakiyStore = createContext()
 
 function Context({ children }) {
-  const [project, setProject] = useState({})
-  const [projects, setProjects] = useState([])
-  const [currentProjectId, setCurrentProjectId] = useState('')
-  const [selectedTheme, setSelectedTheme] = useState('corporate')
+  const [project, setProject] = useState({}) // Current selected project
+  const [projects, setProjects] = useState([]) // List of all projects
+  const [currentProjectId, setCurrentProjectId] = useState('') // Current selected project ID
+  const [selectedTheme, setSelectedTheme] = useState('corporate') // Theme setting
 
-  const [updateFormData, setUpdateFormData] = useState({
+  const [setUpdateFormData] = useState({
     newFinishDate: '',
     newContractSum: '',
     subsequentPayments: '',
     description: '',
   })
 
-  const [expenditures, setExpenditures] = useState([])
-
+  const [expenditures, setExpenditures] = useState([]) // List of expenditures for the selected project
   const [projectSumAndDate, setProjectSumAndDate] = useState({
     projectFinishDate: '',
     projectContractSum: '',
   })
-
   const [workingProjectSumAndDate, setWorkingProjectSumAndDate] = useState({
     workingProjectFinishDate: '',
     workingProjectContractSum: '',
   })
 
-  const [expendituresTrigger, setExpendituresTrigger] = useState({})
+  const [expendituresTrigger, setExpendituresTrigger] = useState({}) // Trigger for fetching expenditures
 
+  // Load the theme from the database or fallback to the default theme
   useEffect(() => {
-    if (projects?.length === 1) {
-      setProject(projects[0])
-    }
-  }, [projects])
-
-  useEffect(() => {
-    const getProjectID = async () => {
-      const res = await getProjects()
-      setProjects(res)
-    }
-    getProjectID()
-  }, [updateFormData])
-
-  useEffect(() => {
-    // Get selected theme from DB
-    const element = document.getElementById('app')
-    const appTheme = async () => {
+    const loadTheme = async () => {
+      const element = document.getElementById('app')
       const savedAppTheme = await getAppTheme()
-      if (!savedAppTheme) {
-        element.setAttribute('data-theme', selectedTheme)
-      } else {
-        element.setAttribute('data-theme', savedAppTheme)
-      }
+      element.setAttribute('data-theme', savedAppTheme || selectedTheme)
     }
-    appTheme()
+    loadTheme()
   }, [selectedTheme])
 
+  // Fetch all projects and set the current project when the app loads
   useEffect(() => {
-    const currentProject = projects?.find(({ id }) => currentProjectId === id)
-    if (currentProject) {
-      // make the new contract sum and new finish date the project's finish date and contract sum if it exists
-      const { new_contract_sum, new_finish_date } = currentProject
+    const loadProjects = async () => {
+      const fetchedProjects = await getProjects()
+      setProjects(fetchedProjects)
 
-      if (new_contract_sum) {
-        setProjectSumAndDate((prevState) => ({
-          ...prevState,
-          projectContractSum: new_contract_sum,
-        }))
-      }
-
-      if (new_finish_date) {
-        setProjectSumAndDate((prevState) => ({
-          ...prevState,
-          projectFinishDate: new_finish_date,
-        }))
-      }
-
-      if (!new_contract_sum) {
-        setProjectSumAndDate((prevState) => ({
-          ...prevState,
-          projectContractSum: currentProject.contract_sum,
-        }))
-      }
-
-      if (!new_finish_date) {
-        setProjectSumAndDate((prevState) => ({
-          ...prevState,
-          projectFinishDate: currentProject.finish_date,
-        }))
+      // Auto-select the first project if only one exists
+      if (fetchedProjects.length === 1) {
+        setProject(fetchedProjects[0])
+        setCurrentProjectId(fetchedProjects[0].id)
       }
     }
-  }, [currentProjectId, projects])
+    loadProjects()
+  }, [])
 
-  useEffect(() => {
-    const workingProject = projects?.find(({ id }) => project.id === id)
-    if (workingProject) {
-      // make the new contract sum and new finish date the project's finish date and contract sum if it exists
-      const { new_contract_sum, new_finish_date } = workingProject
-
-      if (new_contract_sum) {
-        setWorkingProjectSumAndDate((prevState) => ({
-          ...prevState,
-          workingProjectContractSum: new_contract_sum,
-        }))
-      }
-
-      if (new_finish_date) {
-        setWorkingProjectSumAndDate((prevState) => ({
-          ...prevState,
-          workingProjectFinishDate: new_finish_date,
-        }))
-      }
-
-      if (!new_contract_sum) {
-        setWorkingProjectSumAndDate((prevState) => ({
-          ...prevState,
-          workingProjectContractSum: project.contract_sum,
-        }))
-      }
-
-      if (!new_finish_date) {
-        setWorkingProjectSumAndDate((prevState) => ({
-          ...prevState,
-          workingProjectFinishDate: project.finish_date,
-        }))
-      }
-    }
-  }, [project.contract_sum, project.finish_date, project.id, projects])
-
-  useEffect(() => {
-    const fetchExpenditures = async () => {
-      try {
-        const data = await getExpendituresByProjectId(currentProjectId)
-        // Sort the data by `created_at` (assuming it's a valid timestamp format)
-        const sortedData = data.sort((a, b) => new Date(b.created_at) - new Date(a.created_at))
-        // Set the sorted data
-        setExpenditures(sortedData)
-      } catch (error) {
-        console.error('Error fetching expenditures:', error)
-      }
-    }
-    currentProjectId && fetchExpenditures()
-  }, [currentProjectId])
-
-  useEffect(() => {
-    const fetchExpenditures = async () => {
-      try {
-        const data = await getExpendituresByProjectId(currentProjectId)
-        // Sort the data by `created_at` (assuming it's a valid timestamp format)
-        const sortedData = data.sort((a, b) => new Date(b.created_at) - new Date(a.created_at))
-        // Set the sorted data
-        setExpenditures(sortedData)
-      } catch (error) {
-        console.error('Error fetching expenditures:', error)
-      }
-    }
-
-    currentProjectId && fetchExpenditures()
-  }, [expendituresTrigger])
-
+  // Load the current project and set its details
   useEffect(() => {
     const loadCurrentProject = async () => {
-      const currentProjectId = await fetchCurrentProjectId()
-      if (currentProjectId) {
-        // Fetch all projects
-        const projects = await getProjects()
-        // Find the project that matches the currentProjectId
-        const currentProject = projects.find(({ id }) => id === currentProjectId)
-        if (currentProject) {
-          setProject(currentProject) // Set the project with the correct data
-          setCurrentProjectId(currentProjectId) // Set the current project ID
-        }
+      const fetchedCurrentProjectId = await fetchCurrentProjectId()
+      if (fetchedCurrentProjectId) {
+        setCurrentProjectId(fetchedCurrentProjectId) // Set current project ID
+        const matchedProject = projects.find(({ id }) => id === fetchedCurrentProjectId)
+        if (matchedProject) setProject(matchedProject) // Set the project if found
       }
     }
     loadCurrentProject()
-  }, []);
+  }, [projects])
+
+  // Update project sum and date when the current project changes
+  useEffect(() => {
+    const updateProjectDetails = (projectData) => {
+      const { new_contract_sum, new_finish_date, contract_sum, finish_date } = projectData
+      setProjectSumAndDate({
+        projectContractSum: new_contract_sum || contract_sum,
+        projectFinishDate: new_finish_date || finish_date,
+      })
+    }
+
+    const currentProject = projects.find(({ id }) => id === currentProjectId)
+    if (currentProject) updateProjectDetails(currentProject)
+  }, [currentProjectId, projects])
+
+  // Update the working project sum and date based on the selected project
+  useEffect(() => {
+    const updateWorkingProjectDetails = (projectData) => {
+      const { new_contract_sum, new_finish_date, contract_sum, finish_date } = projectData
+      setWorkingProjectSumAndDate({
+        workingProjectContractSum: new_contract_sum || contract_sum,
+        workingProjectFinishDate: new_finish_date || finish_date,
+      })
+    }
+
+    const workingProject = projects.find(({ id }) => id === project.id)
+    if (workingProject) updateWorkingProjectDetails(workingProject)
+  }, [project, projects])
+
+  // Fetch and sort expenditures when the current project or the expenditures trigger changes
+  useEffect(() => {
+    const fetchExpenditures = async () => {
+      if (currentProjectId) {
+        try {
+          const data = await getExpendituresByProjectId(currentProjectId)
+          const sortedData = data.sort((a, b) => new Date(b.created_at) - new Date(a.created_at))
+          setExpenditures(sortedData)
+        } catch (error) {
+          console.error('Error fetching expenditures:', error)
+        }
+      }
+    }
+    fetchExpenditures()
+  }, [currentProjectId, expendituresTrigger])
 
   return (
     <DakiyStore.Provider
       value={{
         project,
         setProject,
-        setProjects,
         projects,
+        setProjects,
         setUpdateFormData,
         projectSumAndDate,
         setProjectSumAndDate,
@@ -195,7 +129,7 @@ function Context({ children }) {
         workingProjectSumAndDate,
         setWorkingProjectSumAndDate,
         expenditures,
-        setExpendituresTrigger
+        setExpendituresTrigger,
       }}
     >
       {children}
